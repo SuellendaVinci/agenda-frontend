2132c88eca6a42472cea664944d3814db1f0f972
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-08-13 03:17:52 -0300

    feat: implement endpoint to get contacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [text, setText] = useState("oi");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/contacts")
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setText("oi, mundo!");
+  }, []);
+
+  return <div>{text}</div>;
 }
 
 export default App;
